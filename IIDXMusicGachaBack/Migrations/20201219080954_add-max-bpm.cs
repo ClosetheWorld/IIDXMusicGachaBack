52b3313130d2f15b288886baf6c5dacb83cfdf90
closetheworld <>
2020-12-19 17:11:50 +0900

    :sparkles: add max bpm

---
+++ IIDXMusicGachaBack/Migrations/20201219080954_add-max-bpm.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IIDXMusicGachaBack.Migrations
+{
+    public partial class addmaxbpm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "MaxBpm",
+                table: "Songs",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxBpm",
+                table: "Songs");
+        }
+    }
+}
